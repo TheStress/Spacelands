86a16d3b80cb03f1e23eabe6c8217e1b2531ab1d
Bexzod Abduvaliev <50254948+BexzodA@users.noreply.github.com>
2021-09-25 14:42:08 -0400

    Zombie animated

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -9,12 +9,14 @@ public class AI : MonoBehaviour
 
     private Rigidbody2D body;
     private Transform trans;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         trans = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,11 +31,34 @@ public class AI : MonoBehaviour
         Vector2 direction = target.position - trans.position;
         if (direction.sqrMagnitude > 1.0f)
         {
-            body.velocity = direction.normalized * speed * Time.deltaTime;
+            Vector2 velocity = direction.normalized * speed * Time.deltaTime;
+            body.velocity = velocity;
+            if(Mathf.Abs(velocity.x) > Mathf.Abs(velocity.y))
+            {
+                if(velocity.x < 0)
+                {
+                    anim.Play("Left");
+                } else
+                {
+                    anim.Play("Right");
+                }
+            }
+            else
+            {
+                if (velocity.y < 0)
+                {
+                    anim.Play("Down");
+                }
+                else
+                {
+                    anim.Play("Up");
+                }
+            }
         }
         else
         {
             body.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            anim.Play("Idle");
         }
     }
 }
