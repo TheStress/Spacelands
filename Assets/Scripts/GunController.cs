70854b9fd3b80ee8257917430290897e5f9376eb
John Zhen <john.zhen24@myhunter.cuny.edu>
2021-09-25 12:38:10 -0400

    updated player and components as prefabs

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject bulletToFire;
+
+    public Transform firePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletToFire, firePoint.position, transform.rotation);
+        }
+    }
+}
