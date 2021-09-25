9685e6907182eaee96e89641d14ca8885af610d3
John Zhen <john.zhen24@myhunter.cuny.edu>
2021-09-25 00:27:12 -0400

    implemented player dash and cursor feature

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,27 +4,113 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    enum Direction {Left, Right, Up, Down};
+
+    private Direction playerDirection = Direction.Right;
     private float playerSpeed = 5.0f;
+    private float dashDistance = 1000f;
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private float dashAnimationDelayTime = 0.2f;
+    private float lastDash = 0.0f;
+    private float dashCooldown = 0.0f;
+    private bool dashing = false;
+
+    public Transform firePoint;
+
+    private Camera mainCam;
+
+    public GameObject bulletToFire;
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
         if (rb == null)
         {
             Debug.LogError("Player is missing a Rigidbody2D component");
         }
+        mainCam = Camera.main;
     }
 
     private void Update()
     {
         Move();
-    }
+        if(Input.GetKeyDown(KeyCode.LeftShift) && Time.time - lastDash > dashCooldown)
+        {
+            dashCooldown = 1f;
+            StartCoroutine(Dash());
+        }
 
+        Vector3 mouse = Input.mousePosition;
+
+        Vector3 screenPoint = mainCam.WorldToScreenPoint(transform.localPosition);
+
+        Vector2 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletToFire, firePoint.position, transform.rotation);
+        }
+
+
+    }
     private void Move()
     {
+
         var horizontalInput = Input.GetAxisRaw("Horizontal");
+        if(horizontalInput < 0)
+        {
+            playerDirection = Direction.Left;
+        }
+        if(horizontalInput > 0)
+        {
+            playerDirection = Direction.Right;
+        }
         var verticalInput = Input.GetAxisRaw("Vertical");
-        rb.velocity = new Vector2(horizontalInput, verticalInput) * (playerSpeed + Time.deltaTime);
+        if(verticalInput < 0)
+        {
+            playerDirection = Direction.Down;
+        }
+        if(verticalInput > 0)
+        {
+            playerDirection = Direction.Up;
+        }
+        rb.velocity = new Vector2(horizontalInput, verticalInput) * playerSpeed;
     }
+
+    IEnumerator Dash()
+    {
+        lastDash = Time.time;
+        sr.color = new Color(1,0,0,1);
+        dashing = true;
+        switch(playerDirection)
+        {
+            case Direction.Left:
+                rb.AddForce(Vector2.left * dashDistance);
+                break;
+            case Direction.Right:
+                rb.AddForce(Vector2.right * dashDistance);
+                break;
+            case Direction.Up:
+                rb.AddForce(Vector2.up * dashDistance);
+                break;
+            case Direction.Down:
+                rb.AddForce(Vector2.down * dashDistance);
+                break;
+            default:
+                Debug.Log("player is not facing any direction");
+                break;
+        }
+        yield return new WaitForSeconds(dashAnimationDelayTime);
+        sr.color = new Color(1,1,1,1);
+        dashing = false;
+    }
+
 }
