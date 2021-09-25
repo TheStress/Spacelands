c5d18694f0f1a472376d611443bc6d4c28922729
Nelson Lieu <lieu.nelson386@gmail.com>
2021-09-25 12:38:06 -0400

    Added enemy spawning and exit room

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Leave the stage
+    }
+}
