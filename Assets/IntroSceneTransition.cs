61e43cc38da6a4647f2289474bb8ca844435c801
RN0529 <richardngowork@gmail.com>
2021-09-25 13:37:15 -0400

    button work
    byutton work

--- Assets/IntroSceneTransition.cs
+++ Assets/IntroSceneTransition.cs
@@ -2,20 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class IntroSceneTransition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void OnMouseDown()
+    public void OnMouseDown()
     {
         Debug.Log("Sprite Clicked");
         SceneManager.LoadScene("Stages Scene");
