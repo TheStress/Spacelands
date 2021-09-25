04dae10171a8fdbd80edff212e1e8c240e015a6c
RN0529 <richardngowork@gmail.com>
2021-09-25 00:26:22 -0400

    script for planet
    script for planet

--- Assets/script.cs
+++ Assets/script.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class script : MonoBehaviour
 {
+    public Transform transform;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform = gameObject.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale += new Vector3(1.0f, 1.0f,0.0f);
     }
 }
