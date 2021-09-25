2d325c389fe81e5770db3e5f5e4ab9639acd3e87
RN0529 <richardngowork@gmail.com>
2021-09-25 15:22:44 -0400

    connected win scene
    win scene

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //Leave the stage
+        SceneManager.LoadScene("Win");
+
     }
 }
