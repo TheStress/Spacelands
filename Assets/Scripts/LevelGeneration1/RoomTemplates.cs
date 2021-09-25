f65815e63084e22a00bb9f5ea1048acbcc41655b
Nelson Lieu <lieu.nelson386@gmail.com>
2021-09-25 12:00:04 -0400

    Added more rooms and layouts for rooms

--- Assets/Scripts/LevelGeneration1/RoomTemplates.cs
+++ Assets/Scripts/LevelGeneration1/RoomTemplates.cs
@@ -8,4 +8,30 @@ public class RoomTemplates : MonoBehaviour
     public GameObject[] topRooms;
     public GameObject[] leftRooms;
     public GameObject[] rightRooms;
+    public GameObject[] roomLayouts;
+
+    public List<GameObject> rooms;
+
+    private int chance;
+
+    private float timer = 2;
+    private bool spawnedLayouts = false;
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0 && spawnedLayouts == false)
+        {
+            AddLayouts();
+            spawnedLayouts = true;
+        }
+    }
+    public void AddLayouts()
+    {
+        for (int i = 1; i < rooms.Count; i++)
+        {
+            chance = Random.Range(0, roomLayouts.Length);
+            Instantiate(roomLayouts[chance], rooms[i].transform.position, rooms[i].transform.rotation);
+        }
+    }
 }
