5a770ebf0ee411bc0f394af6c436c99d12d550fe
Bexzod Abduvaliev <50254948+BexzodA@users.noreply.github.com>
2021-09-25 17:31:23 -0400

    Added damage tint to player.

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     public int health = 100;
+    private SpriteRenderer[] bodies;
+
+    private float clock = 0.0f;
+    private float dmgEffectDuration = 0.25f;
+    private bool damaged = false;
+
+    private Color[] ogColors;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bodies = gameObject.GetComponentsInChildren<SpriteRenderer>();
+        ogColors = new Color[bodies.Length];
+        for (int i = 0; i < bodies.Length; i++)
+        {
+            ogColors[i] = bodies[i].color;
+        }
     }
 
     // Update is called once per frame
@@ -18,10 +30,29 @@ public class Health : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        if(damaged)
+        {
+            if (clock >= dmgEffectDuration)
+            {
+                for(int i = 0; i < bodies.Length; i++)
+                {
+                    bodies[i].color = ogColors[i];
+                }
+                clock = 0.0f;
+                damaged = false;
+            }
+            clock += Time.deltaTime;
+        }
     }
 
     public void TakeDamage(int amount)
     {
         health -= amount;
+        for (int i = 0; i < bodies.Length; i++)
+        {
+            bodies[i].color = Color.red;
+        }
+        damaged = true;
     }
 }
